15d44527b98453831b9d7f0d7d37a58b221985ee
Vuong Minh <vuongxuongminh@gmail.com>
2018-09-19 08:24:50 +0700

    Prepare for test environment

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link https://github.com/vuongxuongminh/migrate-phone-number
+ * @copyright Copyright (c) 2018 Vuong Xuong Minh
+ * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
+ */
+
+namespace VXM\MPN\Tester;
+
+use PHPUnit\Framework\TestCase;
+
+use Symfony\Component\Console\Application;
+
+/**
+ * Lớp trừu tượng BaseTestCase hổ trợ việc cấu hình cơ bản cho các lớp test case.
+ *
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0
+ */
+abstract class BaseTestCase extends TestCase
+{
+
+    /**
+     * @var null|Application
+     */
+    protected $app;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->mockApp();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown()
+    {
+        $this->app = null;
+    }
+
+    /**
+     * Khởi tạo application phục vụ cho việc gắn command test.
+     *
+     * @return Application Đối tượng app phục vụ cho việc test.
+     */
+    protected function mockApp()
+    {
+        return $this->app = new Application('Migrate Phone Number Test', '1.0.0');
+    }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+// ensure we get report on all possible php errors
+error_reporting(E_ALL);
+
+
+require_once(__DIR__ . '/../vendor/autoload.php');
